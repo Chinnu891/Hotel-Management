100eed3437e1e78e88f870aaca98ea72c69abd1a
Chinnu891 <113848035+Chinnu891@users.noreply.github.com>
2026-01-05 18:37:41 +0530

    Delete backend directory

--- backend/rooms/getRoomAvailability.js
+++
@@ -1,8 +0,0 @@
-    // Process the rooms data
-    $all_rooms = array();
-    $available_rooms = array();
-    $occupied_rooms = array();
-    $pending_rooms = array();
-    $booked_rooms = array();
-    
-    foreach ($rooms as $room) {
